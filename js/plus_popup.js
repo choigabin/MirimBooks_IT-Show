1ca5f8f1ef6afe08bff10985bb45a346dbe5bf3d
hajin <rhrnak624@naver.com>
2021-04-20 19:04:08 +0900

    added hover-color change

--- js/plus_popup.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-let link = document.location.href;
-if(link == "https://akwls.github.io/MirimBooks_IT-Show/main.html#plus") {
-    let plus_icon = document.getElementById('plus_icon');
-    plus_icon.style.fill = "#386138";
-}
