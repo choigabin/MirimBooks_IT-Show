13cc139df63c21cba5d6883ed2e63e46a20fdbc1
hajin <rhrnak624@naver.com>
2021-04-20 18:53:24 +0900

    Added plus-icon

---
+++ js/plus_popup.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let link = document.location.href;
+if(link == "https://akwls.github.io/MirimBooks_IT-Show/main.html#plus") {
+    let plus_icon = document.getElementById('plus_icon');
+    plus_icon.style.fill = "#386138";
+}
