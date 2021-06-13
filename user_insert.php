a7405b6321257c2c640d442c8734cdf71c43633d
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-14 08:25:33 +0900

    회원가입 디비 연결

--- user_insert.php
+++ user_insert.php
@@ -4,27 +4,14 @@
     $_num = $_POST['num'];
     $_name = $_POST['name'];
     $_email = $_POST['email'];
+    $_passwd = $_POST['passwd'];
 
-    $sql = "insert into user(num, name, email)
-    values( '$_num', '$_name', '$_email')";
+    $sql = "insert into user(num, name, email, passwd)
+    values( '$_num', '$_name', '$_email', '$_passwd')";
 
     mysqli_query($conn, $sql);
 ?>
 <script>
     alert("미북스의 회원이 되신 것을 환영합니다!");
-    location.href="main.php";
+    location.href="main_login.php";
 </script>
-
-<?php
-$conn = mysqli_connect('localhost', 'root', '100412', 'mibooks');
-
-$_title = $_POST['plus_title'];
-$_writer = $_POST['plus_writer'];
-$_publisher = $_POST['plus_publisher'];
-$_whole = $_POST['plus_whole'];
-
-$sql = "insert into add_book(plus_title, plus_writer, plus_publisher, plus_whole)
-values( '$_title', '$_writer', '$_publisher', '$_whole')";
-
-mysqli_query($conn, $sql);
-?>
