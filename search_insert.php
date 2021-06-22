02a4f95fc67dcdad9228301b0ce7c0e83de6ab9b
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-22 12:36:11 +0900

    도서 정보 Insert Process

--- search_insert.php
+++ search_insert.php
@@ -6,9 +6,10 @@ $_img = $_POST['img'];
 $_teacher = $_POST['teacher'];
 $_title = $_POST['title'];
 $_content = $_POST['content'];
+$_link = $_POST['link'];
 
-$sql = "insert into search(img, teacher, title, content)
-values('$_img', '$_teacher', '$_title', '$_content')";
+$sql = "insert into search(img, teacher, title, content, link)
+values('$_img', '$_teacher', '$_title', '$_content', '$_link')";
 
 mysqli_query($conn, $sql);
 ?>
