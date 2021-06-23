705173d650949ff91f1dd3a73ebfc9fc956d978b
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-23 23:33:14 +0900

    포트폴리오 서버 디비에 저장

--- portfolio_insert.php
+++ portfolio_insert.php
@@ -2,7 +2,7 @@
 <?php
 session_start();
 //데이터 베이스 연결 
-$conn = mysqli_connect('localhost', 'root', '100412', 'mibooks');
+$conn = mysqli_connect('localhost', 'mibooks', 'P9bHVqNsezHNbSfE', 'mibooks');
 
 $_user = $_SESSION['useremail'];
 $_title = $_POST['title'];
@@ -11,13 +11,21 @@ $_publisher = $_POST['publisher'];
 $_date = $_POST['date'];
 $_content = $_POST['content'];
 
+mysqli_query($conn,"set names utf8;");
+
 $sql = "insert into portfolio(user, title, writer, publisher, date, content)
 values('$_user', '$_title', '$_writer', '$_publisher', '$_date', '$_content')";
 
-mysqli_query($conn, $sql);
+///mysqli_query($conn, $sql);
+if (mysqli_query($conn, $sql)) {
+    ?>
+    <script>
+       alert("포트폴리오 작성이 완료되었습니다.")
+       location.href="main.php";
+    </script>
+    <?php
+} else {
+    echo "레코드 추가 실패! : ".mysqli_error($conn);
+}
+mysqli_close($conn);
 ?>
-
-<script>
-    alert("포트폴리오 작성이 완료되었습니다.")
-    location.href="main.php";
-</script>
