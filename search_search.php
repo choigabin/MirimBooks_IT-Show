b8e836d19e2b1013944993c52dfb9d292d1758db
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-23 23:59:13 +0900

    추천 책 리스트, 카테고리 검색 기능

--- search_search.php
+++ search_search.php
@@ -2,57 +2,33 @@
     $search = $_POST['search-name'];
 
     switch($search) {
-        case "WEB", "web":
+        case "WEB":
+        case "web":
             echo "<script>location.href='search.php'</script>";
             break;
-        case "BACK TECH", "back tech":
+        case "BACK TECH":
+        case "back tech":
             echo "<script>location.href='search2.php'</script>";
             break;
         case "자기계발":
             echo "<script>location.href='search3.php'</script>";
             break;
-        case "소설", "에세이", "소설/에세이":
-            echo "<script>location.href='search.php'</script>";
+        case "소설":
+        case "에세이":
+        case "소설/에세이":
+            echo "<script>location.href='search4.php'</script>";
             break;
-        case "고전", "역사", "소설/에세이":
-    }
-  
-    if($search = "WEB" or $search = "web") {
-    ?>ㄴ
-        <script>
-           location.href="search.php";
-        </script>
-    <?php
-    } elseif($search = "BACK TECH" or $search = "back tech") {
-    ?>
-        <script>
-            location.href="search2.php";
-        </script>
-    <?php
-    } elseif($search = "자기계발") {
-    ?>
+        case "고전":
+        case "역사":
+        case "고전 역사":
+            echo "<script>location.href='search5.php'</script>";
+            break;
+        default:
+        ?>
             <script>
-                location.href="search3.php";
-            </script>
-    <?php
-    } elseif($search = "소설" or $search = "에세이" or $search = "소설/에세이") {
-    ?>
-            <script> 
-                location.href="search4.php";
-            </script>
-    <?php
-    } elseif($search = "고전" or $search = "역사" or $search = "고전 역사") {
-    ?>
-            <script>
-                location.href="search5.php";
-            </script>
-    <?php
-    } else {
-    ?>
-        <script>
-            alert("알맞은 카테고리를 입력하세요");
-            location.href="search.php";
-        </script>
-    <?php
+                alert("올바른 카테고리를 입력해주세요");
+                location.href='search.php';
+            </script>;
+        <?php  
     }
 ?>
