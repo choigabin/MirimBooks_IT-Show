fe379bd675952c8afd93ac79493827b1f6c63d2e
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-23 23:27:11 +0900

    인상깊은 구절 서버 디비에 저장

--- phrases_insert.php
+++ phrases_insert.php
@@ -1,17 +1,26 @@
 <!-- 인상깊은 구절 디비 연결 -->
 <?php
 session_start();
-$conn = mysqli_connect('localhost', 'root', '100412', 'mibooks');
+$conn = mysqli_connect('localhost', 'mibooks', 'P9bHVqNsezHNbSfE', 'mibooks');
 
 $_user = $_SESSION['useremail'];
 $_content = $_POST['input-text'];
 
+mysqli_query($conn,"set names utf8;");
+
 $sql = "insert into phrases(user, content)
 values('$_user', '$_content')";
 
-mysqli_query($conn, $sql);
+///mysqli_query($conn, $sql);
+if (mysqli_query($conn, $sql)) {
+    ?>
+    <script>
+       alert("인상깊은 구절 등록이 완료되었습니다!");
+       location.href="main.php";
+    </script>
+    <?php
+} else {
+    echo "레코드 추가 실패! : ".mysqli_error($conn);
+}
+mysqli_close($conn);
 ?>
-<script>
-    alert("인상깊은 구절 등록이 완료되었습니다!");
-    location.href="main.php";
-</script>
