77dbc3c292005b3f10db308d82c0e01618c29360
choigabin <63983505+choigabin@users.noreply.github.com>
2021-06-23 23:26:55 +0900

    입력받은 도서정보를 서버 디비에 저장

--- book_insert.php
+++ book_insert.php
@@ -1,20 +1,29 @@
 <!-- 독서 목록 디비 연결 -->
 <?php
-session_start();
-$conn = mysqli_connect('localhost', 'root', '100412', 'mibooks');
+    session_start();
+    $conn = mysqli_connect('localhost', 'mibooks', 'P9bHVqNsezHNbSfE', 'mibooks');
 
-$_user = $_SESSION['useremail'];
-$_title = $_POST['plus_title'];
-$_writer = $_POST['plus_writer'];
-$_publisher = $_POST['plus_publisher'];
-$_whole = $_POST['plus_whole'];
+    $_user = $_SESSION['useremail'];
+    $_title = $_POST['plus_title'];
+    $_writer = $_POST['plus_writer'];
+    $_publisher = $_POST['plus_publisher'];
+    $_whole = $_POST['plus_whole'];
 
-$sql = "insert into add_book(user, plus_title, plus_writer, plus_publisher, plus_whole)
-values('$_user', '$_title', '$_writer', '$_publisher', '$_whole')";
+    mysqli_query($conn,"set names utf8;");
 
-mysqli_query($conn, $sql);
+    $sql = "insert into add_book(user, plus_title, plus_writer, plus_publisher, plus_whole)
+    values('$_user', '$_title', '$_writer', '$_publisher', '$_whole')";
+
+    ///mysqli_query($conn, $sql);
+    if (mysqli_query($conn, $sql)) {
+        ?>
+        <script>
+        alert("책 등록이 완료되었습니다!");
+        location.href="main.php";
+        </script>
+        <?php
+    } else {
+        echo "레코드 추가 실패! : ".mysqli_error($conn);
+    }
+    mysqli_close($conn);
 ?>
-<script>
-    alert("책 등록이 완료되었습니다!");
-    location.href="main.php";
-</script>
